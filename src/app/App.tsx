fa613e50a8668286ede12acceb00723f15d2ef2d
Valery Zinchenko <minicablestone@gmail.com>
2022-01-29 09:13:59 +0300

    minor fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
 import Localization from "modules/localization/controller"
 import useLocalization from "modules/localization/hook"
 
+import { FAQ, FAQClause } from "./components/common/FAQ/FAQ"
+
 
 function App() {
   const ll = useLocalization(ll => ll)
@@ -9,6 +11,10 @@ function App() {
       <button onClick={() => Localization.transit("ru")}>To russian</button>
       <button onClick={() => Localization.transit("en")}>To english</button>
       {ll?.lang}
+
+      <FAQ>
+        <FAQClause summary="asd">asd</FAQClause>
+      </FAQ>
     </>
   )
 }
